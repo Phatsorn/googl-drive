b6a1b35f2fc5e095577b6a1dbd783396ea0774c9
Pahatsorn <Phatjp41@gmail.com>
2018-05-29 00:15:08 +0700

    first commit

--- server.js
+++ server.js
@@ -3,17 +3,32 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
+let contacts = [
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
 let contacts = []
 
 app.use(bodyParser.json())
 app.use(cors())
 
+
+app.use(bodyParser.json())
+app.use(cors())
+
 /// TODO: Develop GET /contacts
+app.get('/contacts', (req, res) => {
+    res.json(contacts)
+})
 
 
 
 /// TODO: Develop POST /contacts
-
+app.listen(3000, () => {
+    console.log('API Server started at port 3000')
+})
 
 
 app.listen(3000, () => {
